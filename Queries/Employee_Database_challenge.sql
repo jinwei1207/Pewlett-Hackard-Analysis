6460412fad580325a8248713edc07c9d7600527f
Wei Jin <104603177+jinwei1207@users.noreply.github.com>
2022-06-20 12:09:02 -0400

    Update Employee_Database_challenge.sql

--- Queries/Employee_Database_challenge.sql
+++ Queries/Employee_Database_challenge.sql
@@ -13,5 +13,41 @@ SELECT COUNT (unique_titles.title),
 title
 INTO retiring_titles
 FROM unique_titles
+SELECT DISTINCT ON (employees.emp_no) employees.emp_no,
+       employees.emp_no,
+       employees.first_name,
+       employees.last_name,
+       employees.birth_date,
+       dept_manager.from_date,
+       dept_manager.to_date
+INTO mentorship_eligibilty
+FROM employees
+INNER JOIN titles
+ON(employees.emp_no=titles.emp_no)
+WHERE (employees.birth_date='1965-01-01' AND '1965-12-31')
+       AND(dept_manager='9999-01-01')
+ORDER BY emp_no;
 GROUP BY title
 ORDER BY COUNT(unique_titles.title) DESC
+
+-- Deliverable 2
+SELECT DISTINCT ON (employees.emp_no) employees.emp_no,
+       employees.first_name,
+       employees.last_name,
+       employees.birth_date,
+       dept_emp.from_date,
+       dept_emp.to_date,
+       titles.title
+INTO mentorship_eligibilty
+FROM employees
+INNER JOIN dept_emp
+ON (employees.emp_no=dept_emp.emp_no)
+INNER JOIN titles
+ON(employees.emp_no=titles.emp_no)
+WHERE (employees.birth_date BETWEEN '1965/01/01' AND '1965/12/31')
+       AND(dept_emp.to_date = '9999-01-01')
+ORDER BY emp_no;
+
+
+     
+
